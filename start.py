db453e51175e5de38d3b08633c441cbbda2c6f17
josemibnf <40121100+josemibnf@users.noreply.github.com>
2021-01-19 21:38:43 +0100

    Update start.py

--- start.py
+++ start.py
@@ -7,21 +7,23 @@ import api_pb2_grpc
 class RandomCnf(api_pb2_grpc.ServiceServicer):
     def RandomCnf(self, request, context):
         return randomCNF.ok()
+    
+if __name__ == "__main__":
+    print('Starting server.')
+    # create a gRPC server
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    api_pb2_grpc.add_ServiceServicer_to_server(
+        RandomCnf(), server=server
+    )
 
-# create a gRPC server
-server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-api_pb2_grpc.add_ServiceServicer_to_server(
-    RandomCnf(), server=server
-)
+    print('Listening on port 8000.')
+    server.add_insecure_port('[::]:8000')
+    server.start()
 
-print('Starting server. Listening on port 8000.')
-server.add_insecure_port('[::]:8000')
-server.start()
-
-# since server.start() will not block,
-# a sleep-loop is added to keep alive
-try:
-    while True:
-        time.sleep(86400)
-except KeyboardInterrupt:
-    server.stop(0)
+    # since server.start() will not block,
+    # a sleep-loop is added to keep alive
+    try:
+        while True:
+            time.sleep(86400)
+    except KeyboardInterrupt:
+        server.stop(0)
