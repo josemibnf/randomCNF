4eebedfa2372efb2f690d07bbd6678c4564ff7f5
josemibnf <40121100+josemibnf@users.noreply.github.com>
2021-01-20 20:28:40 +0100

    Update start.py

--- start.py
+++ start.py
@@ -1,15 +1,16 @@
-import randomCNF
-from concurrent import futures
-import grpc, time
-from api_pb2 import Cnf
-import api_pb2_grpc
-
-class RandomCnf(api_pb2_grpc.ServiceServicer):
-    def RandomCnf(self, request, context):
-        return randomCNF.ok()
-    
-if __name__ == "__main__":
+   if __name__ == "__main__":
     print('Starting server.')
+    
+    import randomCNF
+    from concurrent import futures
+    import grpc, time
+    from api_pb2 import Cnf
+    import api_pb2_grpc
+
+    class RandomCnf(api_pb2_grpc.ServiceServicer):
+        def RandomCnf(self, request, context):
+            return randomCNF.ok()
+        
     # create a gRPC server
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     api_pb2_grpc.add_ServiceServicer_to_server(
